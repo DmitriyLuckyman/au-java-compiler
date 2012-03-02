b7e4047b2562b5c587288313d1cb1e688f4c07cf
Luckyman <Luckyman@Luckyman-PC>
2012-03-02 23:54:59 +0400

    Change structure

--- src/ru/spbau/compiler/syntax/Parser.java
+++
@@ -1,5 +0,0 @@
-package ru.spbau.compiler.syntax;
-
-public interface Parser {
-    public Program parse() throws SyntaxException;
-}
--- src/ru/spbau/compiler/syntax/Tokenizer.java
+++
@@ -1,7 +0,0 @@
-package ru.spbau.compiler.syntax;
-
-public interface Tokenizer {
-    public String getToken();
-    public String getLine();
-    public int getLineNumber();
-}
